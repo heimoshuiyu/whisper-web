44eb695e7031331e36a4c602ebf3660084de3c55
heimoshuiyu <heimoshuiyu@gmail.com>
2024-01-20 19:26:03 +0800

    move response_format to settings

--- src/App.tsx
+++ src/App.tsx
@@ -101,6 +101,23 @@ function App({ worker, isWorkerReady, setIsWorkerReady, ffmpeg_worker_js_path }:
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text" value={key} onChange={(e) => setKey(e.target.value)} />
           </span>
+          <span className="my-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Response format:
+            </label>
+            <select
+              id="response_format"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              value={selectResponseFormat}
+              onChange={(e) => setSelectResponseFormat(e.target.value)}
+            >
+              <option value="text">Text</option>
+              <option value="srt">SRT</option>
+              <option value="vtt">VTT</option>
+              <option value="stream">stream</option>
+              <option value="json">JSON</option>
+            </select>
+          </span>
         </div>
       )}
 
@@ -118,24 +135,6 @@ function App({ worker, isWorkerReady, setIsWorkerReady, ffmpeg_worker_js_path }:
           }}
         />
       </div>
-
-      <span className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2">
-          Response format:
-        </label>
-        <select
-          id="response_format"
-          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-          value={selectResponseFormat}
-          onChange={(e) => setSelectResponseFormat(e.target.value)}
-        >
-          <option value="text">Text</option>
-          <option value="srt">SRT</option>
-          <option value="vtt">VTT</option>
-          <option value="stream">stream</option>
-          <option value="json">JSON</option>
-        </select>
-      </span>
 
       <div className="mt-6 flex justify-between space-x-4">
         <button
